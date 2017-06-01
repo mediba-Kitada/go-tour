31fec139c9bdeb10b5903d17b46a4cd45afd9e1a
kitatuba <hokudenyoku@gmail.com>
2017-06-02 06:45:31 +0900

    2017/06/02

--- exercise-reader.go
+++ exercise-reader.go
@@ -4,7 +4,11 @@ import "golang.org/x/tour/reader"
 
 type MyReader struct{}
 
-func (mr MyReader) Read([]byte) (int, error) {
+// ASCII文字 'A'の無限ストリームを出力するReader型を実装
+func (mr MyReader) Read(b []byte) (int, error) {
+	// ASCII文字'A'は、引数に格納する。どのように呼び出し元に返しているのか..
+	b[0] = 'A'
+	return 1, nil
 }
 
 func main() {
