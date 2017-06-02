6b42d5ab6f0bcf5f198956cd0789886d421f6ee2
[mediba] Tsubasa Kitada <kitada@mediba.jp>
2017-06-02 15:27:27 +0900

    2017/06/02 late /コメント追加

--- exercise-rot-reader.go
+++ exercise-rot-reader.go
@@ -10,6 +10,7 @@ type rot13Reader struct {
 	r io.Reader
 }
 
+// ROT13換字式暗号関数
 func rot13(p byte) (b byte) {
 	switch {
 	case (p >= 'A' && p < 'N') || (p >= 'a' && p < 'n'):
@@ -25,7 +26,7 @@ func rot13(p byte) (b byte) {
 // ROT13換字式暗号をすべてのアルファベット文字に適用して読み出し
 // Read関数を実装し、io.Readerインターフェースを満たす必要がある
 func (rot *rot13Reader) Read(bs []byte) (int, error) {
-
+	// rot13Reader.rは、io.Reader型なので、Read関数を実装している。この関数をラップする
 	n, e := rot.r.Read(bs)
 	for i := 0; i < n; i++ {
 		bs[i] = rot13(bs[i])
