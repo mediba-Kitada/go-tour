f76c209cd0301a20c975f819f06964549294ba55
kitatuba <hokudenyoku@gmail.com>
2017-06-19 06:40:46 +0900

    2017/06/19 take01

--- exercise-web-crawler.go
+++ exercise-web-crawler.go
@@ -5,11 +5,16 @@ import (
 )
 
 type Fetcher interface {
-	Fetch(url string) (body string, url []string, err error)
+	// Fetch returns the body or URL and a slice of URLs fond on the page.
+	Fetch(url string) (body string, urls []string, err error)
 }
 
+// Crawl uses fetcher to recursively crawl pages starting with url,to a maximum of depth.
 func Crawl(url string, depth int, fetcher Fetcher) {
-	if depth < 0 {
+	// TODO: Fetch URLs in parallel.
+	// TODO: Don't fetch the same URL twice.
+	// This implementation dose't do either:
+	if depth <= 0 {
 		return
 	}
 
@@ -26,5 +31,40 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 }
 
 func main() {
-	Crawl("http://golang.ort/", 4, fetcher)
+	Crawl("http://golang.org/", 4, fetcher)
+}
+
+// fakeFetcher is Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found:%s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"http://golang.org/": &fakeResult{
+		"The Go Programing Language",
+		[]string{"http://golang.org/pkg/", "http://golang.org/cmd/"},
+	},
+	"http://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{"http://golang.org/", "http://golang.org/cmd/", "http://golang.org/pkg/fmt/"},
+	},
+	"http://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{"http://golang.org/", "http://golang.org/pkg/"},
+	},
+	"http://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{"http://golang.org/", "http://golang.org/pkg/"},
+	},
 }
